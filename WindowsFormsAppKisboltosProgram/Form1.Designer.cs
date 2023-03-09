670b96c4eac93e87af1430efdde99be22f00a1a6
Gameplay07HUN <gameplay719@gmail.com>
2023-03-09 09:01:58 +0100

    2.0

--- WindowsFormsAppKisboltosProgram/Form1.Designer.cs
+++ WindowsFormsAppKisboltosProgram/Form1.Designer.cs
@@ -28,20 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBoxKisBolt = new System.Windows.Forms.GroupBox();
+            this.buttonTorles = new System.Windows.Forms.Button();
+            this.buttonModosit = new System.Windows.Forms.Button();
+            this.buttonUj = new System.Windows.Forms.Button();
+            this.numericUpDownMennyiseg = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownEgysegar = new System.Windows.Forms.NumericUpDown();
+            this.textBoxGyumolcsnev = new System.Windows.Forms.TextBox();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.dataGridViewKisBolt = new System.Windows.Forms.DataGridView();
+            this.groupBoxKisBolt.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMennyiseg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEgysegar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKisBolt)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBoxKisBolt
+            // 
+            this.groupBoxKisBolt.Controls.Add(this.buttonTorles);
+            this.groupBoxKisBolt.Controls.Add(this.buttonModosit);
+            this.groupBoxKisBolt.Controls.Add(this.buttonUj);
+            this.groupBoxKisBolt.Controls.Add(this.numericUpDownMennyiseg);
+            this.groupBoxKisBolt.Controls.Add(this.numericUpDownEgysegar);
+            this.groupBoxKisBolt.Controls.Add(this.textBoxGyumolcsnev);
+            this.groupBoxKisBolt.Controls.Add(this.textBoxId);
+            this.groupBoxKisBolt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxKisBolt.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxKisBolt.Name = "groupBoxKisBolt";
+            this.groupBoxKisBolt.Size = new System.Drawing.Size(800, 103);
+            this.groupBoxKisBolt.TabIndex = 3;
+            this.groupBoxKisBolt.TabStop = false;
+            this.groupBoxKisBolt.Text = "Kiválasztott termék adatai";
+            // 
+            // buttonTorles
+            // 
+            this.buttonTorles.Location = new System.Drawing.Point(433, 74);
+            this.buttonTorles.Name = "buttonTorles";
+            this.buttonTorles.Size = new System.Drawing.Size(75, 23);
+            this.buttonTorles.TabIndex = 6;
+            this.buttonTorles.Text = "Törlés";
+            this.buttonTorles.UseVisualStyleBackColor = true;
+            // 
+            // buttonModosit
+            // 
+            this.buttonModosit.Location = new System.Drawing.Point(267, 74);
+            this.buttonModosit.Name = "buttonModosit";
+            this.buttonModosit.Size = new System.Drawing.Size(75, 23);
+            this.buttonModosit.TabIndex = 5;
+            this.buttonModosit.Text = "Módosít";
+            this.buttonModosit.UseVisualStyleBackColor = true;
+            // 
+            // buttonUj
+            // 
+            this.buttonUj.Location = new System.Drawing.Point(80, 74);
+            this.buttonUj.Name = "buttonUj";
+            this.buttonUj.Size = new System.Drawing.Size(75, 23);
+            this.buttonUj.TabIndex = 4;
+            this.buttonUj.Text = "Új áru";
+            this.buttonUj.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDownMennyiseg
+            // 
+            this.numericUpDownMennyiseg.Location = new System.Drawing.Point(624, 30);
+            this.numericUpDownMennyiseg.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.numericUpDownMennyiseg.Name = "numericUpDownMennyiseg";
+            this.numericUpDownMennyiseg.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownMennyiseg.TabIndex = 3;
+            // 
+            // numericUpDownEgysegar
+            // 
+            this.numericUpDownEgysegar.Location = new System.Drawing.Point(433, 31);
+            this.numericUpDownEgysegar.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.numericUpDownEgysegar.Name = "numericUpDownEgysegar";
+            this.numericUpDownEgysegar.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownEgysegar.TabIndex = 2;
+            // 
+            // textBoxGyumolcsnev
+            // 
+            this.textBoxGyumolcsnev.Location = new System.Drawing.Point(174, 31);
+            this.textBoxGyumolcsnev.Name = "textBoxGyumolcsnev";
+            this.textBoxGyumolcsnev.Size = new System.Drawing.Size(168, 20);
+            this.textBoxGyumolcsnev.TabIndex = 1;
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(12, 30);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.ReadOnly = true;
+            this.textBoxId.Size = new System.Drawing.Size(143, 20);
+            this.textBoxId.TabIndex = 0;
+            // 
+            // dataGridViewKisBolt
+            // 
+            this.dataGridViewKisBolt.AllowUserToDeleteRows = false;
+            this.dataGridViewKisBolt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewKisBolt.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridViewKisBolt.Location = new System.Drawing.Point(0, 103);
+            this.dataGridViewKisBolt.Name = "dataGridViewKisBolt";
+            this.dataGridViewKisBolt.ReadOnly = true;
+            this.dataGridViewKisBolt.Size = new System.Drawing.Size(800, 347);
+            this.dataGridViewKisBolt.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBoxKisBolt);
+            this.Controls.Add(this.dataGridViewKisBolt);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Kisboltos Program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxKisBolt.ResumeLayout(false);
+            this.groupBoxKisBolt.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMennyiseg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEgysegar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKisBolt)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxKisBolt;
+        private System.Windows.Forms.Button buttonTorles;
+        private System.Windows.Forms.Button buttonModosit;
+        private System.Windows.Forms.Button buttonUj;
+        private System.Windows.Forms.NumericUpDown numericUpDownMennyiseg;
+        private System.Windows.Forms.NumericUpDown numericUpDownEgysegar;
+        private System.Windows.Forms.TextBox textBoxGyumolcsnev;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.DataGridView dataGridViewKisBolt;
     }
 }
 
